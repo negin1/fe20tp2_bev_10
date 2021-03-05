5ec1c4735d2c68b0546bc8ae3e746fc9b942510e
bannanaz <anlepet@gmail.com>
2021-03-05 15:33:07 +0100

    fetch covid api and create components for CovidCards, CovidChart & CovidCountry

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,36 @@
 import React from 'react';
 
 import { withAuthorization } from '../Session';
+import Cards from '../CovidCards';
+import Chart from '../CovidChart';
+import Country from '../CovidCountry';
+import fetchCovidData from '../../constants/covidapi';
 
-const HomePage = () => (
-  <div>
-    <h1>Home Page</h1>
-    <p>The Home Page is accessible by every signed in user.</p>
-  </div>
-);
+class HomePage extends React.Component {
+
+  state = {
+    data: {},
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchCovidData();
+    this.setState({ data: fetchedData });
+  }
+
+  render() {
+    const { data } = this.state;
+
+    return (<div>
+      <h1>Home Page</h1>
+      <p>The Home Page is accessible by every signed in user.</p>
+      <Cards data={data} />
+      <Chart />
+      <Country />
+
+    </div>
+    )
+  }
+};
 
 const condition = authUser => !!authUser;
 
