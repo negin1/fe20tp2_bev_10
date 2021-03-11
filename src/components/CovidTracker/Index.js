4a537fe8991ffcdc0434d17edea17468466c054c
negin1 <negin.jalalian@gmail.com>
2021-03-11 16:16:47 +0100

    added the dates, country picker,  chart not working

--- src/components/CovidTracker/Index.js
+++ src/components/CovidTracker/Index.js
@@ -1,59 +1,87 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-import LineGraph from "./LineGraph";
-import CovidSummary from "./CovidSummary";
-import axios from './axios';
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import LineGraph from './LineGraph'
+import CovidSummary from './CovidSummary'
+import axios from './axios'
 
 const StyledDiv = styled.div`
   text-align: center;
-`;
+`
 
 function CovidTracker() {
-
-  const [totalConfirmed, setTotalConfirmed] = useState(0);
-  const [totalRecovered, setTotalRecovered] = useState(0);
-  const [totalDeaths, setTotalDeaths] = useState(0);
-  const [loading, setLoading] = useState(false);
-  const [covidSummary, setCovidSummary] = useState({});
-  const [days, setDays] = useState(7);
-  const [country, setCountry] = useState('');
+  const [totalConfirmed, setTotalConfirmed] = useState(0)
+  const [totalRecovered, setTotalRecovered] = useState(0)
+  const [totalDeaths, setTotalDeaths] = useState(0)
+  const [loading, setLoading] = useState(false)
+  const [covidSummary, setCovidSummary] = useState({})
+  const [days, setDays] = useState(7)
+  const [country, setCountry] = useState('')
+  const [coronaCountAr, setCoronaCountAr] = useState([])
 
   //ComponentDidMount
   useEffect(() => {
-
     //setLoading(true);
-    axios.get(`/summary`)
-      .then(res => {
+    axios
+      .get(`/summary`)
+      .then((res) => {
         //setLoading(false);
 
         if (res.status === 200) {
-          setTotalConfirmed(res.data.Global.TotalConfirmed);
-          setTotalRecovered(res.data.Global.TotalRecovered);
-          setTotalDeaths(res.data.Global.TotalDeaths);
-          setCovidSummary(res.data);
+          setTotalConfirmed(res.data.Global.TotalConfirmed)
+          setTotalRecovered(res.data.Global.TotalRecovered)
+          setTotalDeaths(res.data.Global.TotalDeaths)
+          setCovidSummary(res.data)
         }
 
-        console.log(res);
+        console.log(res)
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error)
       })
+  }, [])
 
-
-  }, []);
+  const fromatDate = (date) => {
+    const d = new Date(date)
+    //2021-03-09;
+    const year = d.getFullYear()
+    const month = `0${d.getMonth() + 1}`.slice(-2)
+    const _date = d.getDate()
+    return `${year}-${month}-${_date}`
+  }
 
   const countryHandler = (e) => {
-    setCountry(e.target.value);
+    setCountry(e.target.value)
+    const d = new Date()
+    const to = fromatDate(d)
+    const from = fromatDate(d.setDate(d.getDate() - 7))
+
+    //console.log(from, to)
+    getCoronaReportByDateRange(e.target.value, from, to)
   }
 
   const daysHandler = (e) => {
-    setDays(e.target.value);
+    setDays(e.target.value)
+  }
+
+  const getCoronaReportByDateRange = (countrySlug, from, to) => {
+    axios.get(
+        `/country/${countrySlug}/status/confirmed?from=${from}T00:00:00Z&to={to}T00:00:00Z`
+      )
+      .then((res) => {
+        console.log(res)
+
+        const yAxisCoronaCount = res.data.map((d) => d.Cases)
+        setCoronaCountAr(yAxisCoronaCount)
+      })
+
+      .catch((error) => {
+        console.log(error)
+      })
   }
 
   if (loading) {
-    return <p> Fetchind gata from api...</p>
+    return <p> Fetchind gata from api ...</p>
   }
-
 
   return (
     <StyledDiv>
@@ -66,21 +94,22 @@ function CovidTracker() {
 
       <div>
         <select value={country} onChange={countryHandler}>
-          {
-            covidSummary.Countries && covidSummary.Countries.map(country =>
-              <option key={country.Slug} value={country.Slug}>{country.Country}</option>
-            )
-          }
+          {covidSummary.Countries &&
+            covidSummary.Countries.map((country) => (
+              <option key={country.Slug} value={country.Slug}>
+                {country.Country}
+              </option>
+            ))}
         </select>
-        <select value={days} onChange={daysHandler} >
-          <option value="7">Last 7 days</option>
-          <option value="30">Last 30 days</option>
-          <option value="90">Last 90 days</option>
+        <select value={days} onChange={daysHandler}>
+          <option value='7'>Last 7 days</option>
+          <option value='30'>Last 30 days</option>
+          <option value='90'>Last 90 days</option>
         </select>
       </div>
-      <LineGraph />
+      <LineGraph yAxis={coronaCountAr} />
     </StyledDiv>
-  );
+  )
 }
 
-export default CovidTracker;
+export default CovidTracker
--- src/components/CovidTracker/LineGraph.js
+++ src/components/CovidTracker/LineGraph.js
@@ -34,7 +34,7 @@ const LineGraph = (props) => {
             pointHoverBorderWidth: 2,
             pointRadius: 1,
             pointHitRadius: 10,
-            data: [65, 59, 80, 81, 56, 55, 40]
+            data: props.yAxis
           }
         ]
       }}  />
