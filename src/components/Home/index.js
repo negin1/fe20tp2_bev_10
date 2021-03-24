b1bb383ca3b995ae8d2b61a5229221996eb5aaf2
negin1 <negin.jalalian@gmail.com>
2021-03-24 17:43:16 +0100

    mob seession- error messsage fixed

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Covid from '../Covid/Covid'
 import CovidTracker from '../CovidTracker/Index'
 import Weather from '../Weather/index'
+/* import GroupGraph from '../CovidTracker/GroupGraph' */
 
 import PresetCovid from '../PresetCovid/Index'
 
@@ -62,7 +63,7 @@ class HomePage extends React.Component {
        <Cards data={data} />
       <Chart />
       <Country /> 
-
+   {/*    <GroupGraph /> */}
       <Weather/>
 
     </div>
