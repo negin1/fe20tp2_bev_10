58128ea95ff82f87d9b24dfddd0d654446d00dd7
ivan olowo <ivanolowo@ivans-MacBook-Pro.local>
2021-03-31 11:55:19 +0200

    updated landing page

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React from 'react';;
+import styled from 'styled-components';
 
 import { FirebaseContext } from '../Firebase';
+/* import { LANDING } from '../../constants/routes'; */
+import { findByLabelText } from '@testing-library/react';
+import { LandingContainer, LandingContent, LandingH1 } from './LandingElements';
 
-const Landing = () => (
+
+const LANDING = () => {
+  return (
+    <>
+      <LandingContainer>
+        <LandingContent>
+          <LandingH1>Keep a track of the Corona virus statistics close to you and around the world.</LandingH1>
+        </LandingContent>
+      </LandingContainer>
+
+    </>
+  )
+}
+
+export default LANDING;
+
+
+
+
+
+/* const LANDING = () => (
   <FirebaseContext.Consumer>
-  {firebase => {
-    return
-     <div>I've access to Firebase and render something.</div>;
+    {firebase => {
+      return
+      <main>
+        <img src={image} alt="Globe Picture" />
+        <section>
+          <h1>Keep track of Corona virus statistics close to you and around the world.</h1>
+        </section>
+
+      </main>
     }}
   </FirebaseContext.Consumer>
 );
-export default Landing;
+export default Landing;  */
+
