4b2b9462a556dc6b8139b9f20d4e0bfaf6e57450
bannanaz <anlepet@gmail.com>
2021-04-13 17:04:28 +0200

    graphlist to local storage

--- src/components/Home/FetchData.js
+++ src/components/Home/FetchData.js
@@ -7,15 +7,36 @@ import Graph from './Graph';
 const FetchData = (props) => {
     const [data, setData] = useState(null)
     const timePeriod = daysHandler(props.days)
-    console.log(timePeriod)
-    console.log(props)
+    const [country, setCountry] = useState('');
+    const [graph, setGraph] = useState('');
+    const [type, setType] = useState('');
+    const [days, setDays] = useState('');
+
+
     // read saved graphs from localstorage
     // push props as an object into the array
     // save the array to LS
     //const [country, setCountry] = useState('');
     //gör ett api anrop
+
+
+    let graphList = [],
+
+        graphObj = {
+            "country": country,
+            "graph": graph,
+            "type": type,
+            "data": data,
+            id: Date.now(),
+        }
+
+    graphList.push(graphObj);
+
+    localStorage.setItem('allGraphs', JSON.stringify(graphList))
+
+
     useEffect(() => {
-        console.log(`https://api.covid19api.com/country/${props.country}/status/${props.type}?from=${timePeriod.from}T00:00:00Z&to=${timePeriod.to}T00:00:00Z`)
+
         axios.get(
             `https://api.covid19api.com/country/${props.country}/status/${props.type}?from=${timePeriod.from}T00:00:00Z&to=${timePeriod.to}T00:00:00Z`
         )
@@ -26,14 +47,20 @@ const FetchData = (props) => {
 
                 const dataCount = data.map((d) => d.Cases)
                 const labels = data.map(d => d.Date)
+                const country = props.country
+                const graph = props.graph
+                const type = props.type
+
                 setData({ dataCount, labels })
+                setGraph(graph)
+                setCountry(country)
+                setType(type)
             })
 
             .catch((error) => {
                 //console.log(error)
-            })
+            });
 
-            ;
     }, [props.country, props.type, props.days, props.graph]);
     return (
         data ? <Graph data={data} country={props.country} type={props.type} graph={props.graph} /> : null
