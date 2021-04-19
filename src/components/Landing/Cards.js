0bccd42ee7a4619fe6244ed1e9526a89e2cf6197
bannanaz <anlepet@gmail.com>
2021-04-19 15:04:04 +0200

    cards on landing updated

--- src/components/Landing/Cards.js
+++ src/components/Landing/Cards.js
@@ -7,7 +7,7 @@ const StyledDiv = styled.div`
   box-sizing: 0 0 10px 0 #eee;
   max-width: 140px;
   height: 80px;
-`
+`;
 
 const Cards = (props) => {
     return <StyledDiv>{props.children}</StyledDiv>
