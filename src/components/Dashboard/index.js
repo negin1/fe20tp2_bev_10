1ef3eb5d7895a3e40ee5b04043849679b97a0252
Ivan Olwo <ivolowo@gmail.com>
2021-04-20 12:45:46 +0200

    added dashboard buttons

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -12,9 +12,6 @@ const Dashboard = () => {
     let graphStr = localStorage.getItem('allGraphs');
     graphList = JSON.parse(graphStr);
 
-<<<<<<< HEAD
-    if (!localStorage.getItem('allGraphs')) { return []; }
-=======
     let MortalityList = []
     let mortalityStr = localStorage.getItem('MortalityGraph');
     MortalityList = JSON.parse(mortalityStr);
@@ -23,28 +20,11 @@ const Dashboard = () => {
     let graphCompareStr = localStorage.getItem('allCompareGraphs');
     graphCompareList = JSON.parse(graphCompareStr);
 
->>>>>>> b051e88fa971af8a0a8eb1b5d68bdd4550bcfced
     //setGraphList(graphList); // update the state if taskList has data
 
     //}, []);
     // ändra formatet som sparas i localstorage. Kolla hur FetchData får props.#eeeeee
     // mappa igenom precis som ni gör nu fast skicka till FetchData istället
-<<<<<<< HEAD
-    /*if
-    ((!localStorage.getItem('allGraphs')) && (!localStorage.getItem('allCompareGraphs'))) { return (<StyledPageIntro><h2>My Dashboard</h2><p>You haven't saved any graphs yet.</p></StyledPageIntro>) }*/
-    return (
-
-        <StyledPageIntro>
-            <h2>My Dashboard</h2 >
-            <p>View your graphs.</p>
-            <div>
-                {graphStr && graphList.map((item, index) => (
-                    <div key={index}>
-                        <p>{item.type} in {item.country}</p>
-                        <FetchData saved={true} {...item} />
-                        {/* <Graph data={item.data} country={item.country} type={item.type} days={item.days} graph={item.graph} /> */}
-=======
->>>>>>> b051e88fa971af8a0a8eb1b5d68bdd4550bcfced
 
     //if (!localStorage.getItem('allGraphs') && !localStorage.getItem('MortalityGraph') ) { return <StyledPageIntro />, []; }
     return (
@@ -77,11 +57,7 @@ const Dashboard = () => {
                 </div>
             </StyledPageIntro>
             <BottomNav />
-<<<<<<< HEAD
-        </StyledPageIntro >
-=======
         </div>
->>>>>>> b051e88fa971af8a0a8eb1b5d68bdd4550bcfced
     )
 }
 
